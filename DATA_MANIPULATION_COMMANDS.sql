78bbafe1ed5fabce0ade8d9b0b6bcc64ae1cdd2e
ShekharNarayanan <shekharnarayanan833@gmail.com>
2025-11-06 13:25:02 +0100

    Add extra comments and DELETE/TRUNCATE statements

--- DATA_MANIPULATION_COMMANDS.sql
+++ DATA_MANIPULATION_COMMANDS.sql
@@ -17,16 +17,43 @@ VALUES
 */
 
 -- inserting to a table from another --
-
--- select data from src table--
-INSERT INTO persons (id, person_name, birth_data, phone)
-SELECT 
+/*
+INSERT INTO persons (id, person_name, birth_data, phone) -- table where data is to be injected --
+SELECT -- columns of the second table that correspond to columns of the src table
 	id,
 	first_name,
 	NULL,
-	'Unknown'
-FROM customers
+	'Unknown' -- add UNKNOWN if the entries from certain column dont correspond between the tables
+FROM customers -- table whose data is being injected into another table
 
 
 -- check results
 SELECT * from persons
+*/
+
+-- UPDATE existing table --
+/*
+UPDATE customers 
+SET 
+	country = 'USA'
+WHERE id = 7
+*/
+
+-- UPDATE multple rows (replace score 0 by 50) --
+/*
+UPDATE customers
+SET score = 50
+WHERE score = 0
+
+SELECT *
+FROM customers
+*/
+
+-- DELETE rows --
+/*
+DELETE FROM customers
+WHERE id > 5
+*/
+
+-- TRUNCATE deletes all data from a table MUCH faster than DELETE --
+TRUNCATE TABLE persons
